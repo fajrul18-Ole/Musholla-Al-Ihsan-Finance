723257b2d7c9aa2d44b8474d1e6203534864905a
Ahmad Fajrul Itsnaini <aWX711583@china.huawei.com>
2026-09-25 11:44:33 +0700

    Fix DB port configuration

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ db_pool = pooling.MySQLConnectionPool(
     pool_size=5,
 
     host=os.environ.get("DB_HOST"),
-    port=int(os.environ.get("DB_PORT", 3306)),
+    port=int(os.environ.get("DB_PORT") or "3306"),
     user=os.environ.get("DB_USER"),
     password=os.environ.get("DB_PASSWORD"),
     database=os.environ.get("DB_NAME"),
